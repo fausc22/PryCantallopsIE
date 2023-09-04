8adf3b3caf803dde25347e7a4c1d1ae2ff3b075d
root <mgiraudo@ies21.edu.ar>
2023-09-04 12:25:11 -0300

    Agregar archivos de proyecto.

---
+++ frmMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryCantallopsIE
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolFecha_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int contador = 0;
+        private void timerHora_Tick(object sender, EventArgs e)
+        {
+            contador += 1000;
+            toolFecha.Text = DateTime.Now.ToLongDateString();
+            toolHora.Text = Convert.ToString(DateTime.Now);
+        }
+    }
+}
